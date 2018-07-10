2edb479b92136e5156f93b1a86661e610a347d2d
leviscar <liujunpeng1994@foxmail.com>
2018-07-10 10:06:57 +0800

    first commit

---
+++ assets/MapRes/Role1Ride.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Role1Ride" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="Role1Ride.png" width="128" height="37"/>
+</tileset>
