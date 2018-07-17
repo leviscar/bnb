48ea7ffe7003f0948c0d21d34c6c305c176bbc5a
leviscar <liujunpeng1994@foxmail.com>
2018-07-17 11:53:51 +0800

    finish bomb put button

---
+++ assets/Scripts/components/map/control/bombBtn.js
@@ -0,0 +1,51 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        let duration = 0.04;
+        this.node.on("touchend",function () {
+            let scaleAction = cc.sequence(cc.scaleTo(duration,1.2),cc.scaleTo(duration,1));
+            this.node.runAction(scaleAction);
+            this.putBomb();
+        },this)
+    },
+    putBomb: function (params) {
+        console.log("bomd down");
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
