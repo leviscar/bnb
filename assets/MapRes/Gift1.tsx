bd65c4e7040f7c5bb819578058c20720bb3deb2d
leviscar <liujunpeng1994@foxmail.com>
2018-07-18 21:06:48 +0800

    remove useless pics

--- assets/MapRes/Gift1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Gift1" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="Gift1.png" width="32" height="36"/>
-</tileset>
