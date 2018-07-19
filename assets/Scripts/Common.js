80f4fb4baed23759f12d99aae487b14b4f56dfe8
leviscar <liujunpeng1994@foxmail.com>
2018-07-19 17:27:11 +0800

    background adapt

--- assets/Scripts/Common.js
+++ assets/Scripts/Common.js
@@ -25,7 +25,7 @@ module.exports = {
     map: {
         basicMap: []
     },
-    host: 'localhost',
-    // host:'212.64.17.36',
+    // host: 'localhost',
+    host:'212.64.17.36',
     port: 4000
 }
