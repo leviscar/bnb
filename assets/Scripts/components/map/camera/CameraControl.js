8203bcc875fe55d85928c73b1d01f63fbc1a4ac7
leviscar <liujunpeng1994@foxmail.com>
2018-07-25 13:31:33 +0800

    欢乐坦克大战类似移动方式

--- assets/Scripts/components/map/camera/CameraControl.js
+++ assets/Scripts/components/map/camera/CameraControl.js
@@ -10,20 +10,33 @@ cc.Class({
         camera: cc.Camera,
         smoothFollow: false,
         centerAtStart: false,
+        followX: 0,
+        followY: 0,
+        minFollowDist: 0,
+        followRatio:0
     },
     onLoad () {
-        this.startFollow = false;
+        this.startFollow = true;
         let canvas = cc.find('Canvas').getComponent(cc.Canvas);
         this.visibleSize = cc.view.getVisibleSize();
         // this.initZoomRatio = this.camera.zoomRatio;
         console.log("camera start");
-        // this.target = cc.find("map").getChildByName(com.socket.role);
-        this.target = cc.find("map").getChildByName("challenger");
+        this.target = cc.find("map").getChildByName(com.socket.role);
+        // this.target = cc.find("map").getChildByName("challenger");
         this.map = cc.find("map");
+        this.map.x = 0;
+        this.map.y = 0;
+        this.actionList = {
+            "down": this.node.position = cc.p(this.visibleSize.width/2-this.map.x,this.visibleSize.height/2-this.map.y),
 
-        if(this.centerAtStart){
-            this.node.position = this.target.convertToNodeSpaceAR(cc.Vec2.ZERO);
         }
+        console.log(this.map.x+","+this.map.y);
+        console.log(this.visibleSize)
+        // this.map.position = cc.p(0,0) ;
+
+        // if(this.centerAtStart){
+        //     this.node.position = this.target.convertToNodeSpaceAR(cc.Vec2.ZERO);
+        // }
         // this.previousPos = this.node.position;
     },
 
@@ -34,6 +47,17 @@ cc.Class({
     onDisable: function () {
         cc.director.getPhysicsManager().detachDebugDrawFromCamera(this.camera);    
     },
+    // 判定角色落在地图的哪个区域
+    transLocate: function (pos) {
+        let dotLeftUp = {
+            x: this.visibleSize.width - this.map.x,
+            y: com.map.basicMap.length*32 - this.visibleSize.height/2 
+        }
+        let dotLeftDown = {
+            x: this.visibleSize.width - this.map.x,
+            y: this.visibleSize.height/2 
+        }
+    },
     lateUpdate: function (dt) {
         try {
             let targetPos,parentNode;
@@ -41,7 +65,7 @@ cc.Class({
             // console.log(parentNode);
             // targetPos = parentNode.convertToWorldSpaceAR(this.target.position);
             targetPos = this.target.position;
-            console.log(targetPos);
+            // console.log(targetPos);
             //smooth follow
             if (this.smoothFollow) {
                 if (Math.abs(targetPos.x - this.node.x) >= this.followX ||
@@ -55,10 +79,24 @@ cc.Class({
                     }
                 }
             } else {
-                // this.node.position = this.node.parent.convertToNodeSpaceAR(targetPos);
+                // console.log(this.visibleSize.height-this.map.y);
+                // if(targetPos.y > (this.visibleSize.height-this.map.y)){
+                //     this.node.position = targetPos;
+                // };
+                // console.log(targetPos-this.map.y-this.visibleSize.height/2);
+                // if((targetPos.y+this.map.y <= this.visibleSize.height/2)){
+                //     // this.node.position = cc.p(this.visibleSize.width/2-this.map.x,this.visibleSize.height/2-this.map.y);
+                //     this.node.position = cc.p(this.visibleSize.width/2-this.map.x,this.visibleSize.height/2-this.map.y)
+                // }else if(targetPos.y+this.visibleSize.height/2>=com.map.basicMap.length*32){
+                //     console.log( cc.p(this.visibleSize.width/2-this.map.x,com.map.basicMap.length*32-this.visibleSize.height/2));
+                //     this.node.position = cc.p(targetPos.x,com.map.basicMap.length*32-this.visibleSize.height/4);
+                //     // this.node.position = cc.p(this.visibleSize.width/2-this.map.x,com.map.basicMap.length*32-this.visibleSize.height/4);
+                // }else{
+                //     this.node.position = targetPos;
+                // }
                 this.node.position = targetPos;
-                // this.map.position = targetPos;
-                // console.log(this.node.position);
+                
+                // this.node.position = targetPos;
             }
     
             this.previousPos = targetPos;
@@ -67,6 +105,4 @@ cc.Class({
         }
 
     }
-
-    // update (dt) {},
 });
