48528df4842336f494bd5a29986585a5f70a39c9
Robbie Xie <619835217@qq.com>
2018-07-30 15:43:42 +0800

    添加subdomain，提供排行榜功能

---
+++ assets/Scripts/components/map/endRoom/GameOverRank.js
@@ -0,0 +1,50 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        rankingScrollView: cc.Sprite
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start: function(){
+        if (CC_WECHATGAME) {
+            window.wx.showShareMenu({withShareTicket: true});//设置分享按钮，方便获取群id展示群排行榜
+            this.tex = new cc.Texture2D();
+            window.sharedCanvas.width = 1000;
+            window.sharedCanvas.height = 300;
+
+            window.wx.postMessage({// 发消息给子域
+                messageType: 4,
+                MAIN_MENU_NUM: "x1"
+            });
+        } else {
+            cc.log("获取横向展示排行榜数据。x1");
+        }
+    },
+
+    // 刷新子域的纹理
+    _updateSubDomainCanvas() {
+        if (window.sharedCanvas != undefined) {
+            this.tex.initWithElement(window.sharedCanvas);
+            this.tex.handleLoadedTexture();
+            this.rankingScrollView.spriteFrame = new cc.SpriteFrame(this.tex);
+        }
+    },
+    update() {
+        this._updateSubDomainCanvas();
+    },
+  
+});
