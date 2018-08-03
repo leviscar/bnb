5506af370a3409930ba056d57f6f209f96a618e2
leviscar <liujunpeng1994@foxmail.com>
2018-08-03 14:34:45 +0800

    首页增加排行榜

--- assets/Scripts/components/start/RankList.js
+++ assets/Scripts/components/start/RankList.js
@@ -11,7 +11,35 @@ cc.Class({
         this.node.on('fade-in',this.show,this);
     },
 
+    start: function(){
+        if (CC_WECHATGAME) {
+            this.tex = new cc.Texture2D();
+            window.sharedCanvas.width = 720;
+            window.sharedCanvas.height = 1280;
+
+            window.wx.postMessage({// 发消息给子域
+                messageType: 1,
+                MAIN_MENU_NUM: "x1"
+            });
+        } else {
+            cc.log("获取横向展示排行榜数据。x1");
+        }
+    },
+    
     show: function() {
         this.node.active = !this.node.active;
-    }
+    },
+
+    // 刷新子域的纹理
+    _updateSubDomainCanvas() {
+        if (window.sharedCanvas != undefined) {
+            this.tex.initWithElement(window.sharedCanvas);
+            this.tex.handleLoadedTexture();
+            this.rankListPanel.spriteFrame = new cc.SpriteFrame(this.tex);
+        }
+    },
+
+    update() {
+        this._updateSubDomainCanvas();
+    },
 });
